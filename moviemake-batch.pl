13367c3b75c16dceb7764d51f830d4c085c3b2f0
Evgenii Baldin <E.Baldin@gmail.com>
2014-07-19 22:00:25 +0200

    add HEP Serbo

---
+++ moviemake-batch.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl -w
+# -------------------------------------------
+# --          use perl                     --
+#$ -S /usr/bin/perl
+# -------------------------------------------
+# --             batch name                --
+#$ -N makemovie
+# -------------------------------------------
+# --     What to redirect to where         --
+#$ -cwd
+#log_$JOB_NAME.$JOB_ID
+#$ -o /dev/null
+#$ -j y
+# -------------------------------------------
+# --             Qeue list                 --
+# default, vm_q
+#$ -q default
+# -------------------------------------------
+# --             mail me                   --
+# -M E.M.Baldin@gmail.com
+#$ -m be
+# -------------------------------------------
+use Getopt::Std;
+use File::Find;
+use File::stat;
+
+getopts("hl:e:");
+use vars qw/$opt_h $opt_l $opt_e/;
+
+if (defined($opt_h)||(!defined($ENV{SGE_TASK_ID}))) {
+  print "Usage: $0 [-l lectureslist.txt] [-e ext] (for batch only!)\n";
+  print "Options: -l - lectures list (def. lectureslist.txt)\n";
+  print "Options: -e - output file extention (def. mp4) \n";
+  print "Options: -h - help message\n";
+  exit();
+}
+
+unless (defined $opt_l) {$opt_l="lectureslist.txt";}
+unless (defined $opt_e) {$opt_e="mp4";}
+
+
+open(LIST,"$opt_l") or die "LIST open: \"$opt_l\" $!";
+while (<LIST>) {
+  chomp;s/#.*//;s/^\s+//;s/\s+$//;next unless length;
+  ($ifile,$dir,$file)=split(/\s+/);
+  if ($ifile<=0) {last;}
+  $dirlist[$ifile]=$dir;
+  $filelist[$ifile]=$file;
+}
+close(LIST) or die "LIST close: \"$opt_l\" $!";
+
+$ifile=$ENV{SGE_TASK_ID};
+
+$cmd="cd ".$dirlist[$ifile]."; make ".$filelist[$ifile].".$opt_e";
+print "$cmd\n";
+system($cmd);
+
--- moviemake.pl
+++ moviemake.pl
@@ -1,302 +1,48 @@
 #!/usr/bin/perl -w
-# Костыль для использования кластера
-# -------------------------------------------
-# --          use perl                     --
-#$ -S /usr/bin/perl
-# -------------------------------------------
-# --             batch name                --
-#$ -N makemovie
-# -------------------------------------------
-# --     What to redirect to where         --
-#$ -cwd
-#log_$JOB_NAME.$JOB_ID
-#$ -o /dev/null
-#$ -j y
-# -------------------------------------------
-# --             Qeue list                 --
-# default, vm_q
-#$ -q default
-# -------------------------------------------
-# --             mail me                   --
-# -M E.M.Baldin@gmail.com
-#$ -m be
-# -------------------------------------------
 use Getopt::Std;
-use File::Find;
-use File::stat;
 
-getopts("hc");
-use vars qw/$opt_h $opt_c/;
-
+getopts("ht:a");
+use vars qw/$opt_h $opt_t $opt_a/;
 
 
 if (defined($opt_h)||
-    (!defined($opt_c)&&!defined($ENV{SGE_TASK_ID}))) {
-  print "Usage: $0 [-h] -f <run> -t <run> [-w <dir>] [-i]\n";
-  print "Options: -c - count number of files\n";
+    (!defined($opt_t)&&!defined($opt_a))) {
+  print "Usage: $0 [-h] [-a || -t ABBR]\n";
   print "Options: -h - help message\n";
+  print "Options: -a - convert all lectures\n";
+  print "Options: -t ABBR - convert specific course\n";
+  print "Options:    ABBR could be QCD, QED, SUSY, EW, NaCD and HEP \n";
   exit();
 }
 
-# список файлов
-@FILES = (
-	  {
-	   file => "QCD_Fadin_Lecture01",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture02",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture03",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture04",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture05",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture06",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture07",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture08",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture09",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture10",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture11",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture12",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture13",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture14",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture15",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QCD_Fadin_Lecture16",
-	   dir => "QCD_Lectures_Fadin_Spring_2014",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture01",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture02",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture03",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture04",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture05",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture06",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture07",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture08",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture09",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture10",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture11",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture12",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture13",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture14",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "QED_Fadin_Lecture15",
-	   dir => "QED_Lectures_Fadin_Autumn_2013",
-	  },
-	  {
-	   file => "SUSY_Chernyak_Lecture19",
-	   dir => "SUSY_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "SUSY_Chernyak_Lecture20",
-	   dir => "SUSY_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "SUSY_Chernyak_Lecture21",
-	   dir => "SUSY_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "SUSY_Chernyak_Lecture22",
-	   dir => "SUSY_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "SUSY_Chernyak_Lecture23",
-	   dir => "SUSY_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "SUSY_Chernyak_Lecture24",
-	   dir => "SUSY_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "SUSY_Chernyak_Lecture25",
-	   dir => "SUSY_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture01",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture02",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture03",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture04",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture05",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture06",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture07",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture08",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture09",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture10",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture11",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture12",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture13",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture14",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture15",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture16",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "EW_Chernyak_Lecture17",
-	   dir => "EW_Lectures_Chernyak_Autumn_2013",
-	  },
-	  {
-	   file => "CHAOS_Zhirov_Lecture01",
-	   dir => "Nonlinear_and_Chaotic_Dynamics_Lectures_Zhirov_Spring_2014",
-	  },
-	  {
-	   file => "CHAOS_Zhirov_Lecture02",
-	   dir => "Nonlinear_and_Chaotic_Dynamics_Lectures_Zhirov_Spring_2014",
-	  },
-	  {
-	   file => "CHAOS_Zhirov_Lecture03",
-	   dir => "Nonlinear_and_Chaotic_Dynamics_Lectures_Zhirov_Spring_2014",
-	  },
-	  {
-	   file => "CHAOS_Zhirov_Lecture04",
-	   dir => "Nonlinear_and_Chaotic_Dynamics_Lectures_Zhirov_Spring_2014",
-	  },
-	 );
+if (defined $opt_a) {$opt_t="ALL"};
 
+# due to lectureslist.txt
+%first = (
+	  ALL => 1,
+	  QCD => 1,
+	  QED => 17,
+	  SUSY => 32,
+	  EW => 39,
+	  NaCD => 56,
+	  HEP => 60,
+);
 
-if (defined $opt_c) {
-  $count=$#FILES+1;
-  printf "$count\n";
-  exit(0);
+%last = (
+	  ALL => 74,
+	  QCD => 16,
+	  QED => 31,
+	  SUSY => 38,
+	  EW => 55,
+	  NaCD => 59,
+	  HEP => 74,
+	);
+
+if (!defined $first{$opt_t}||!defined $last{$opt_t}) {
+  die "Undefined $opt_t";
 }
 
-#delta=1;#ENV{SGE_TASK_STEPSIZE};
-#nfile=$ENV{SGE_TASK_ID}-1;
-$nfile=int(($ENV{SGE_TASK_ID}-1)/4);
-$sparse=($ENV{SGE_TASK_ID}-1)%4;
-# sparce используется, чтобы на восьмиядерном узле одновременно
-# запускалось по возможности только два потока. Костыль.
-if ($sparse==0) {
-  $cmd="cd ".$FILES[$nfile]{dir}."; make ".$FILES[$nfile]{file}.".mp4";
-  print "$cmd\n";
-  system($cmd);
-}
-else {
-  sleep(120);
-}
+$cmd="qsub -pe pvm 4 -t $first{$opt_t}-$last{$opt_t} ./moviemake-batch.pl";
+print "$cmd\n";
+#system($cmd);
 
