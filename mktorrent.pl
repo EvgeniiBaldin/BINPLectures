a7b953ff9d76b8237b832fa4b6f79ae660f65118
Evgeny Baldin <E.M.Baldin@inp.nsk.su>
2014-12-02 09:53:22 +0600

    QCD->$item

--- mktorrent.pl
+++ mktorrent.pl
@@ -4,17 +4,17 @@ $bin="transmission-create ";
 
 foreach $item (keys %dir) {
 # 1080p tracker
-  $cmd="cd ".$basepath{"1080p"}."; $bin --comment \"".$comment1080p{$item}."\" --outfile $torrentfilename1080p{$item} ";
+  $cmd="cd ".$basepath{"1080p"}."; $bin --comment \"$comment1080p{$item}\" --outfile $torrentfilename1080p{$item} ";
   for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
-  $cmd.="$dir{QCD} ";
+  $cmd.="$dir{$item} ";
 
   print "$cmd\n";
   system($cmd);
 
 # src tracker
-  $cmd="cd ".$basepath{"src"}."; $bin --comment \"".$commentsrc{$item}."\" --outfile $torrentfilenamesrc{$item} ";
+  $cmd="cd ".$basepath{"src"}."; $bin --comment \"$commentsrc{$item}\" --outfile $torrentfilenamesrc{$item} ";
   for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
-  $cmd.="$dir{QCD} ";
+  $cmd.="$dir{$item} ";
 
   print "$cmd\n";
   system($cmd);
