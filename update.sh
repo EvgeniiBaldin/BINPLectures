3740629ff4f0a8c459eb757d085f15d2f1ba6768
Evgeny Baldin <E.M.Baldin@inp.nsk.su>
2015-05-24 15:47:34 +0600

    update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,20 @@
+./moviemake.pl -c TO
+./moviemake.pl -c ANTAR
+./moviemake.pl -c QCDs -f 12 -t 20
+
+#./sendtoyoutube.pl -c TO
+#./sendtoyoutube.pl -c ANTAR
+#./sendtoyoutube.pl -c QCDs -f 10 -t 20
+
 #./moviemake.pl -c ASTRO
 #./moviemake.pl -c QCDs
 #./moviemake.pl -c HEPs -f 16 -t 17
 #./moviemake.pl -c GG -f 12 -t 17
 #./moviemake.pl -c NUC -f 11 -t 16
 
+
 #./sendtoyoutube.pl -c ASTRO 
-./sendtoyoutube.pl -c QCDs
+#./sendtoyoutube.pl -c QCDs
 #./sendtoyoutube.pl -c HEPs -f 16 -t 17
 #./sendtoyoutube.pl -c GG -f 12 -t 17
 #./sendtoyoutube.pl -c NUC -f 11 -t 16
