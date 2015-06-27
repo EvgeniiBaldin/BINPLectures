4f031515073414e47ebfa5bc2fdb19b623216fb2
Evgeny Baldin <E.M.Baldin@inp.nsk.su>
2015-06-27 22:14:46 +0600

    tracker for rutracker and openbittorrent

--- mktorrent.pl
+++ mktorrent.pl
@@ -4,28 +4,50 @@ $bin="transmission-create ";
 
 
 foreach $item (keys %dir) {
-#foreach $item (qw(HEP)) {
 # 720p tracker
+# rutracker
   $cmd="cd $torrentpath ; $bin --comment \"$comment720p{$item}\" --outfile $torrentfilename720p{$item}\[torrents.ru\].torrent ";
-#  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
   $cmd.="\"$dir720p{$item}\" ";
+  print "$cmd\n";
+  system($cmd);
 
+# 720p tracker
+# https://openbittorrent.com/
+  $cmd="cd $torrentpath ; $bin --comment \"$comment720p{$item}\" --outfile $torrentfilename720p{$item}.torrent ";
+  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
+  $cmd.="\"$dir720p{$item}\" ";
+  print "$cmd\n";
+  system($cmd);
+
+
+# 1080p tracker
+# rutracker
+  $cmd="cd $torrentpath ; $bin --comment \"$comment1080p{$item}\" --outfile $torrentfilename1080p{$item}\[torrents.ru\].torrent ";
+#  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
+  $cmd.="\"$dir1080p{$item}\" ";
   print "$cmd\n";
   system($cmd);
 
 # 1080p tracker
-  $cmd="cd $torrentpath ; $bin --comment \"$comment1080p{$item}\" --outfile $torrentfilename1080p{$item}\[torrents.ru\].torrent ";
-#  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
+# https://openbittorrent.com/
+  $cmd="cd $torrentpath ; $bin --comment \"$comment1080p{$item}\" --outfile $torrentfilename1080p{$item}.torrent ";
+  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
   $cmd.="\"$dir1080p{$item}\" ";
-
   print "$cmd\n";
   system($cmd);
 
 # src tracker
+# rutracker
   $cmd="cd $torrentpath ; $bin --comment \"$commentsrc{$item}\" --outfile $torrentfilenamesrc{$item}\[torrents.ru\].torrent ";
-#  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
   $cmd.="\"$dirsrc{$item}\" ";
+  print "$cmd\n";
+  system($cmd);
 
+# src tracker
+# https://openbittorrent.com/
+  $cmd="cd $torrentpath ; $bin --comment \"$commentsrc{$item}\" --outfile $torrentfilenamesrc{$item}\[torrents.ru\].torrent ";
+  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
+  $cmd.="\"$dirsrc{$item}\" ";
   print "$cmd\n";
   system($cmd);
 }
