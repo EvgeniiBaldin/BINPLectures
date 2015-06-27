606521465538152b847cc5ab8cd1b502e088d875
Evgeny Baldin <E.M.Baldin@inp.nsk.su>
2015-06-27 22:24:05 +0600

    add dirr for rutracker

--- mktorrent.pl
+++ mktorrent.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 require data;
 $bin="transmission-create ";
-
+$rutrackerdir="../rutracker";
 
 foreach $item (keys %dir) {
 # 720p tracker
 # rutracker
-  $cmd="cd $torrentpath ; $bin --comment \"$comment720p{$item}\" --outfile $torrentfilename720p{$item}\[torrents.ru\].torrent ";
+  $cmd="cd $torrentpath ; $bin --comment \"$comment720p{$item}\" --outfile $rutrackerdir/$torrentfilename720p{$item}\[torrents.ru\].torrent ";
   $cmd.="\"$dir720p{$item}\" ";
   print "$cmd\n";
   system($cmd);
@@ -22,7 +22,7 @@ foreach $item (keys %dir) {
 
 # 1080p tracker
 # rutracker
-  $cmd="cd $torrentpath ; $bin --comment \"$comment1080p{$item}\" --outfile $torrentfilename1080p{$item}\[torrents.ru\].torrent ";
+  $cmd="cd $torrentpath ; $bin --comment \"$comment1080p{$item}\" --outfile $rutrackerdir/$torrentfilename1080p{$item}\[torrents.ru\].torrent ";
 #  for ($i=0;$i<=$#trackerlist;$i++) {$cmd.="--tracker $trackerlist[$i] "}
   $cmd.="\"$dir1080p{$item}\" ";
   print "$cmd\n";
@@ -38,7 +38,7 @@ foreach $item (keys %dir) {
 
 # src tracker
 # rutracker
-  $cmd="cd $torrentpath ; $bin --comment \"$commentsrc{$item}\" --outfile $torrentfilenamesrc{$item}\[torrents.ru\].torrent ";
+  $cmd="cd $torrentpath ; $bin --comment \"$commentsrc{$item}\" --outfile $rutrackerdir/$torrentfilenamesrc{$item}\[torrents.ru\].torrent ";
   $cmd.="\"$dirsrc{$item}\" ";
   print "$cmd\n";
   system($cmd);
