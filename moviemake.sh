e09130967c44d579cc2a027548b7a1a0eb6542b3
Evgenii Baldin <E.M.Baldin@inp.nsk.su>
2021-02-02 16:23:35 +0700

    fix description on Covers

--- moviemake.sh
+++ moviemake.sh
@@ -1,5 +1,5 @@
 date
 #./moviemake.pl -c TO17 -f 1 -t 4
-./moviemake.pl -c ToSI -f 1 -t 20
-#./moviemake.pl -c ToSI -f 20 -t 20
+./moviemake.pl -c ToSI -f 18 -t 19
+#./moviemake.pl -c ToSI -f 1 -t 20
 date
